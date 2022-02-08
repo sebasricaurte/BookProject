93dff2541ad352e7a4c33e122ccb8b33788b2765
Juan Sebastian Ricaurte Gonzalez <sebastian.ricaurte@udea.edu.com>
2022-02-08 00:10:36 -0500

    sesión 3.1

--- app/src/main/java/com/ricaurte/bookproject/MainActivity.kt
+++ app/src/main/java/com/ricaurte/bookproject/MainActivity.kt
@@ -1,11 +1,150 @@
 package com.ricaurte.bookproject
 
-import androidx.appcompat.app.AppCompatActivity
+import android.app.DatePickerDialog
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.ricaurte.bookproject.databinding.ActivityMainBinding
+import java.text.SimpleDateFormat
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var mainBinding: ActivityMainBinding // declare variable
+    private var cal = Calendar.getInstance()
+    private var publicationDate = ""
+
+    override fun onBackPressed() {
+        super.onBackPressed()
+        Toast.makeText(this, "Bye Bye", Toast.LENGTH_SHORT).show()
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        Log.d("state","onCreate")
+        mainBinding = ActivityMainBinding.inflate(layoutInflater)
+
+        setContentView(mainBinding.root)
+
+        val dateSetListener = DatePickerDialog.OnDateSetListener { _, year, month, day0fMOnth ->
+            cal.set(Calendar.YEAR, year)
+            cal.set(Calendar.MONTH, month)
+            cal.set(Calendar.DAY_OF_MONTH, day0fMOnth)
+
+            val format = "dd-MM-yyyy"
+            val simpleDateFormat = SimpleDateFormat(format, Locale.US)
+            publicationDate = simpleDateFormat.format(cal.time).toString()
+            mainBinding.publicationDateButton.text = publicationDate
+        }
+
+        with(mainBinding) {
+
+            publicationDateButton.setOnClickListener {
+                DatePickerDialog(
+                    this@MainActivity,
+                    dateSetListener,
+                    cal.get(Calendar.YEAR), //coja el calendanrio del año actual
+                    cal.get(Calendar.MONTH),
+                    cal.get(Calendar.DAY_OF_MONTH)
+                ).show() // mostrarlo
+
+            }
+
+            saveButton.setOnClickListener {
+
+                if (nameBookEditText.text?.isEmpty() == true ||
+                    nameAuthorEditText.text?.isEmpty() == true ||
+                    pagesEditText.text?.isEmpty() == true
+                ) { //condicionales, pregutnar si el namebook esta vacio || or
+                    Toast.makeText(
+                        applicationContext,
+                        "Debe digitar nombre, autor y número de págians",
+                        Toast.LENGTH_SHORT
+                    ).show()
+
+                } else {
+
+                    val nameBook: String = nameBookEditText.text.toString()
+                    val author = nameAuthorEditText.text.toString()
+                    val pages = pagesEditText.text.toString().toInt()
+                    // para covertir enteros primeros es string
+                    val abstract = abstractEditText.text.toString()
+
+                    var genre = ""
+
+                    if (suspenseCheckBox.isChecked) genre = "Suspenso"
+                    if (terrorCheckBox.isChecked) genre += "Terror"
+                    if (infantileCheckBox.isChecked) genre += "Infantil"
+                    if (fictionCheckBox.isChecked) genre += "Ficcion"
+
+                    //   var score = if (oneRadioButton.isChecked) 1 else 2
+                    val score = when {
+                        oneRadioButton.isChecked -> 1
+                        twoRadioButton.isChecked -> 2
+                        threeRadioButton.isChecked -> 3
+                        fourRadioButton.isChecked -> 4
+                        else -> 5
+                    }
+
+                    infoTextView.text =
+                        getString(R.string.info, nameBook, author, pages, abstract, genre, score, publicationDate) // para visulizarlo
+
+                }
+            }
+
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_overflow, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId){
+            R.id.menu_sign_out -> goToLoginActivity()
+
+        }
+        return  super.onOptionsItemSelected(item)
+    }
+
+    private fun goToLoginActivity() {
+        val intent = Intent(this, LoginActivity::class.java)
+        intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK  or Intent.FLAG_ACTIVITY_CLEAR_TASK
+        startActivity(intent)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d("state","onStart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d("state","onResume")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d("state","onPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d("state","onStop")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d("state","onDestroy")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.d("state","onRestart")
     }
 }
